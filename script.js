fe5cd4d291a3e097fe1ded1c591f18cb852dc17b
Razy <97766343+razyness@users.noreply.github.com>
2024-09-12 02:48:39 +0200

    the old italian trick of dropping every file

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 window.addEventListener("load", function () {
-    document.getElementById("blurry-mess").style.opacity = "0";
+  document.getElementById("blurry-mess").style.opacity = "0";
 
-    // Set a timeout of 200ms and dellete the blurry mess div entirely
-    setTimeout(function () {
-        document.getElementById("blurry-mess").remove();
-    }, 200);
+  setTimeout(function () {
+    document.getElementById("blurry-mess").remove();
+  }, 200);
 });
+
+fetch("https://api.github.com/users/razyness")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    const avatarUrl = data.avatar_url;
+    const ghAvatarElement = document.getElementById("gh-avatar");
+
+    if (ghAvatarElement) {
+      ghAvatarElement.src = avatarUrl;
+    }
+
+    const faviconLink =
+      document.querySelector("link[rel~='icon']") ||
+      document.createElement("link");
+    faviconLink.rel = "icon";
+    faviconLink.href = `https://images.weserv.nl/?url=${avatarUrl}&fit=cover&mask=circle&maxage=7d`;
+
+    if (!faviconLink.parentNode) {
+      document.head.appendChild(faviconLink);
+    }
+  });
